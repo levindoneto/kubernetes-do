4b32ddf0399a200e916052628232b515dd66e818
Levindo Neto <levindogtn@gmail.com>
2019-01-22 21:24:57 -0200

    Add shell script with the steps used for configuring the master

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# kubernetes-do
-Repository containing some theory about Kubernetes and how to set up a master and a node using it on Digital Ocean
+# Kubernetes DO Configuration
+
+Repository containing some theory about Kubernetes and how to set up a master and a node using it on Digital Ocean infrastructure provider.
+
+**Author:** Levindo Gabriel Taschetto Neto.
+
+## License
+
+MIT License. Click [here](LICENSE.md) for more information about this license.
+
---
+++ master-config.sh
@@ -0,0 +1,39 @@
+# Cluster/Orchestration Manager
+# Set up on root@<IP_MACHINE_HERE>
+# Levindo G. T. Neto
+# Kubernetes version: v1.13.2
+# Updated in January 2019
+
+export MASTER_IP=<IP_MACHINE_HERE>
+apt-get update && apt-get upgrade -y 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update -y
+apt-get install -y docker.io
+# Check the docker version
+docker version
+apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+# master config -> for using a virtual networking layer (flannel)
+# Generate the certificates and token for adding nodes to this master later on
+kubeadm init --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address $MASTER_IP
+
+# Always needed
+# If these commands are not run you'd get:
+# 'The connection to the server localhost:8080 was refused
+#  - did you specify the right host or port?'
+cp /etc/kubernetes/admin.conf $HOME/
+chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+
+# Get Flannel
+curl -sSL "https://github.com/coreos/flannel/blob/master/Documentation/kube-flannel.yml?raw=true" | kubectl --namespace=kube-system create -f -
+
+# Install dashboard
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml --namespace=kube-system
+
+# To check out the created pods
+kubectl get pods --all-namespaces
