e09bb26e919832187ca48fcc76caf8dbafb84e33
Levindo Neto <levindogtn@gmail.com>
2019-01-23 00:13:36 -0200

    Add shell script with the steps used for configuring a node

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Repository containing some theory about Kubernetes and how to set up a master an
 
 **Author:** Levindo Gabriel Taschetto Neto.
 
+## Set Up a Master
+
+## Set Up a Node
+
+## Result
+
+![node-1_to_master-1](resources/node-1_to_master-1.png)
+
 ## License
 
 MIT License. Click [here](LICENSE.md) for more information about this license.
---
+++ node-config.sh
@@ -0,0 +1,20 @@
+# Cluster/Orchestration Manager
+# Set up of a Node on root@<IP_MACHINE_HERE>
+# Levindo G. T. Neto
+# Kubernetes version: v1.13.2
+# Updated in January 2019
+
+apt-get update && apt-get upgrade -y 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update -y
+apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+
+# Join to the created master (this command is given at the master at the end of the setup, just copy it and paste it below
+kubeadm join <IP_MACHINE_HERE>:6443 --token <GENEREATED_TOKEN_1> --discovery-token-ca-cert-hash sha256:49<GENEREATED_TOKEN_2>
+
+
+#27d4cc1dc5813692c29d0a26b1
